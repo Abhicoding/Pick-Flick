995e90c026ce3a4d86e151b6d8e3fbb22cd208c7
Abhicoding <shinganeabhishek@gmail.com>
2018-06-29 10:44:43 +0530

    Added images to the movie card

--- src/components/MovieCard/moviecard.jsx
+++ src/components/MovieCard/moviecard.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 
 const MovieCard = (movie) => {
+  let trimmed
+  if (movie.title.length > 24) {
+    trimmed = movie.overview.substring(0, 88) + ' ...'
+  } else {
+    trimmed = movie.overview.substring(0, 120) + ' ...'
+  }
   return (
     <Wrapper>
       <div className='tile is-ancestor'>
         <div className='tile is-parent'>
           <article className='tile is-child box' >
-            <figure className='image is-4by3'>
-              <img src='https://bulma.io/images/placeholders/640x480.png' />
-            </figure>
+            <img src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`} />
             <p className='title'>{movie.title}</p>
             <p className='subtitle'>{movie.vote_average}</p>
             <div className='content'>
-              <p>{movie.overview}</p>
+              <p>{trimmed}</p>
             </div>
           </article>
         </div>
@@ -25,8 +29,8 @@ const MovieCard = (movie) => {
 const Wrapper = styled.div`
 float: left;
 width: 30%;
-hegith: 40%;
+height: 730px;
 margin-left: 2.5%;
-margin-top: 2%`
+margin-top: 2%;`
 
 export default MovieCard

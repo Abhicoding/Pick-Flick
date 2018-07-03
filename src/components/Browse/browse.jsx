3c76480ff628647f0573392a4fee5c7c6bbd1315
Abhicoding <shinganeabhishek@gmail.com>
2018-07-03 10:42:08 +0530

    type checked browse

--- src/components/Browse/browse.jsx
+++ src/components/Browse/browse.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+// @flow
+import * as React from 'react'
 
 import MovieCard from '../MovieCard/moviecard.jsx'
 
-const Browse = (data) => {
+const Browse = (data: Data) => {
   return (
     data.results.map((movie) =>
       <MovieCard key={movie.id} {...movie} />
@@ -10,3 +11,28 @@ const Browse = (data) => {
 }
 
 export default Browse
+
+type Data = {
+  "page": number,
+  "total_results": number, 
+  "total_pages": number, 
+  "results": Array<Movie>,
+}
+
+type Movie = {
+  "vote_count": number, 
+  "id": number, 
+  "video": boolean, 
+  "vote_average": number, 
+  "title": string, 
+  "popularity": number, 
+  "poster_path": string, 
+  "original_language": string, 
+  "original_title": string, 
+  "genre_ids": Array<number>, 
+  "backdrop_path": string, 
+  "adult": boolean, 
+  "overview": string, 
+  "release_date": string
+
+}
